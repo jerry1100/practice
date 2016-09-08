56a8431ecc68ab30afcc903e843421af0b025363
Jerry1100 <jwu1100@gmail.com>
2016-09-08 13:48:45 -1000

    3ms solution -- no help

---
+++ 342_power_of_four.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfFour(int num);
+// Given: a signed 32 bit integer
+// Returns: whether it is a power of 4
+
+int main()
+{
+    int i;
+    for (i = 0; i < 0b1111111111111111111111111111111; i++)
+    {
+        if (isPowerOfFour(i))
+        {
+            printf("%d is a power of four\n", i);
+        }
+    }
+}
+
+bool isPowerOfFour(int num)
+{
+    switch (num) // test individually for powers of 4
+    {
+        case 0b00000000000000000000000000000001:
+        case 0b00000000000000000000000000000100:
+        case 0b00000000000000000000000000010000:
+        case 0b00000000000000000000000001000000:
+        case 0b00000000000000000000000100000000:
+        case 0b00000000000000000000010000000000:
+        case 0b00000000000000000001000000000000:
+        case 0b00000000000000000100000000000000:
+        case 0b00000000000000010000000000000000:
+        case 0b00000000000001000000000000000000:
+        case 0b00000000000100000000000000000000:
+        case 0b00000000010000000000000000000000:
+        case 0b00000001000000000000000000000000:
+        case 0b00000100000000000000000000000000:
+        case 0b00010000000000000000000000000000:
+        case 0b01000000000000000000000000000000: return true;
+        default: return false;
+    }
+}
