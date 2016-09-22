5c8969e998982cffbe420c9563f7986db16e18bf
Jerry1100 <jwu1100@gmail.com>
2016-09-22 11:04:16 -1000

    0ms solution --no help

--- 66_plus_one.c
+++ 66_plus_one.c
@@ -1,3 +1,9 @@
+/*
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
