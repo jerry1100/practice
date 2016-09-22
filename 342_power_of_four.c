c9f2c02a1a612ea44404e2bad63b2296b3336f86
Jerry1100 <jwu1100@gmail.com>
2016-09-22 11:03:04 -1000

    3ms solution --no help

--- 342_power_of_four.c
+++ 342_power_of_four.c
@@ -1,3 +1,12 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 
