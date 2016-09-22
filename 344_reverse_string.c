030cf3a85fd12ea5a193c25cda3d7f7b66bfaac5
Jerry1100 <jwu1100@gmail.com>
2016-09-22 10:59:12 -1000

    3ms solution --no help

--- 344_reverse_string.c
+++ 344_reverse_string.c
@@ -1,3 +1,10 @@
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+*/
+
 #include <stdio.h>
 
 char *reverseString(char *s);
