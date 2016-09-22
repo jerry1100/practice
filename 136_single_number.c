133866243e9756d0d268dd8d82b7c658fa883de6
Jerry1100 <jwu1100@gmail.com>
2016-09-22 11:00:33 -1000

    6ms solution --with help

--- 136_single_number.c
+++ 136_single_number.c
@@ -1,3 +1,10 @@
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
 #include <stdio.h>
 
 int singleNumber(int *nums, int numsSize);
