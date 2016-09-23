479b5699c12b619483db3308661a3f399e32a20b
Jerry1100 <jwu1100@gmail.com>
2016-09-22 14:31:32 -1000

    Easy 0ms solution --no help

---
+++ 371_sum_of_two_integers.c
@@ -0,0 +1,54 @@
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+*/
+
+#include <stdio.h>
+
+int getSum(int a, int b);
+// Given: two integers
+// Returns: the sum of the two integers without using + or - operators
+
+int main() {
+    int num1, num2;
+
+    for (;;) {
+        printf("Enter two numbers separated by a space: ");
+        scanf("%d %d", &num1, &num2);
+        printf("The sum of %d and %d is %d\n\n", num1, num2, getSum(num1, num2));
+    }
+}
+
+int getSum(int a, int b) {
+    int sum = 0;
+    int carry = 0;
+    long int mask;
+    for (mask = 1; mask <= 2147483648; mask <<= 1) { // check all bits starting from end
+        int bitA = a & mask;
+        int bitB = b & mask;
+
+        // Possible combinations
+        if (carry) {
+            if (bitA & bitB) { // two 1s and carry
+                sum |= mask;
+                carry = 1;
+            } else if (bitA ^ bitB) { // one 1 and carry
+                carry = 1;
+            } else { // just carry
+                sum |= mask;
+                carry = 0;
+            }
+        } else {
+            if (bitA & bitB) { // two 1s
+                carry = 1;
+            } else if (bitA ^ bitB) { // one 1
+                sum |= mask;
+            } else { // nothing
+                carry = 0;
+            }
+        }
+    }
+    return sum;
+}
