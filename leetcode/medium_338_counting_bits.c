8696b2031bf4c4832566d07359fccf3dbb588860
Jerry1100 <jwu1100@gmail.com>
2016-10-24 21:32:30 -1000

    Changed filenames to lowercase

---
+++ leetcode/medium_338_counting_bits.c
@@ -0,0 +1,21 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0  i  num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+*/
+
+int *countBits(int num, int *returnSize);
+// Given: two integers
+// Returns: an array and saves the size of the array in returnSize
+
+int main() {
+    for (;;) {
+        int num;
+        printf("Enter a number: %d
+    }
+}
+
+int *countBits(int num, int *returnSize) {
+    int *arr = (int *) malloc(num*(sizeof(int)));
+}
