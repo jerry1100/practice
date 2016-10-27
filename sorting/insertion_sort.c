2f1e106547d4010192c2ad8104cbe77b3ba27ce6
Jerry1100 <jwu1100@gmail.com>
2016-10-26 22:21:08 -1000

    added new input method for insertion

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -4,62 +4,44 @@ Insertion sort algorithm to sort an array of integers
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void insertionSort(int *nums, int size);
 // Given: array of integers and length of array
 // Array elements will be sorted in ascending order
 
-// First parameter: size of randomly generated array
-// Second parameter: 1 to display array contents, 0 to suppress
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Error: 2 arguments needed, %d given\n", argc-1);
-        return -1;
-    }
+int main() {
+    for (;;) {
+        int arr[1000];
+        int size = 0;
 
-    srand(time(NULL));
-    int size = atoi(argv[1]);
-    int display = atoi(argv[2]);
-    int numArr[size];
-    int i;
+        // Read in numbers and save to array
+        printf("Enter array elements, separate by spaces:\n");
+        char ch;
+        while (scanf("%d%c", &arr[size++], &ch) && ch != '\n');
 
-    // Populate array with random numbers 1 - 100
-    for (i = 0; i < size; i++) {
-        numArr[i] = rand() % 100 + 1;
-    }
-
-    // Unsorted
-    if (display) {
-        printf("Before:\t[ ");
+        // Unsorted
+        printf("Unsorted: {");
+        int i;
         for (i = 0; i < size; i++) {
-            printf("%d ", numArr[i]);
+            printf(" %d ", arr[i]);
         }
-        printf("]\n\n");
-    }
+        printf("}\n");
 
-    // Measure runtime
-    clock_t clk = clock();
-    insertionSort(numArr, size);
-    clk = clock() - clk;
-
-    // Sorted
-    if (display) {
-        printf("After:\t[ ");
+        // Sorted
+        insertionSort(arr, size);
+        printf("Sorted: {");
         for (i = 0; i < size; i++) {
-            printf("%d ", numArr[i]);
+            printf(" %d ", arr[i]);
         }
-        printf("]\n");
+        printf("}\n\n");
     }
-
-    printf("Process took %li clock cycles\n", clk);
 }
 
 void insertionSort(int *nums, int size) {
     int i, j;
-    for (i = 1; i < size; i++) {
-        for (j = i; nums[j] < nums[j-1] && j > 0; j--) {
-            int temp = nums[j];
+    for (i = 1; i < size; i++) { // go through elements, starting at second
+        for (j = i; nums[j] < nums[j-1] && j > 0; j--) { // find correct spot
+            int temp = nums[j]; // swap elements
             nums[j] = nums[j-1];
             nums[j-1] = temp;
         }
