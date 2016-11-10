4863969d869b1b4ce5ebd1ede80f83d13b338dd5
Jerry1100 <jwu1100@gmail.com>
2016-11-10 13:40:47 -1000

    restructured file folders

---
+++ leetcode/medium_005_longest_palindromic_substring.c
@@ -0,0 +1,18 @@
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+#include <stdio.h>
+
+char *longestPalindrome(char *s);
+// Given: a string of characters
+// Returns: the largest substring in the string that is a palindrome
+
+int main() {
+    char s[] = "Wassup dawg\n";
+    longestPalindrome(s);
+}
+
+char *longestPalindrome(char *s) {
+    
+}
