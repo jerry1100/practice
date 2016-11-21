2fc02d7b143915c0e0ee48fe1eb47a89208fa770
Jerry1100 <jwu1100@gmail.com>
2016-11-20 14:00:12 -1000

    added dec to bin

---
+++ random/dec_to_bin.c
@@ -0,0 +1,40 @@
+/*
+A simple decimal to binary converter
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *dec_to_bin(int num);
+// Given: an integer
+// Returns: a string that is the binary representation of the integer
+
+int main() {
+    for (;;) {
+        int num;
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        printf("%d in binary is: %s\n\n", num, dec_to_bin(num));
+    }
+}
+
+char *dec_to_bin(int num) {
+    // Find the number of bits needed
+    int tmp = num;
+    int num_bits;
+    for (num_bits = 0; tmp != 0; num_bits++) {
+        tmp /= 2;
+    }
+
+    char *bin_str = (char *) malloc((num_bits+1)*sizeof(char));
+
+    // Fill in binary string in reversed order
+    int i;
+    for (i = num_bits-1; i >= 0; i--) {
+        bin_str[i] = num % 2 + '0';
+        num /= 2;
+    }
+    bin_str[num_bits] = '\0';
+
+    return bin_str;
+}
