8860511c9926e528267281a3c41aae0c2f8016bd
Jerry1100 <jwu1100@gmail.com>
2017-01-28 00:09:31 -1000

    Updated progress

--- random/run_background.c
+++ random/run_background.c
@@ -6,10 +6,10 @@ Simple program to print out numbers in the background
 #include <unistd.h>
 
 int main() {
-    int i = 0;
-
-    for (;;) {
-        printf("%d\n", i++);
+    int i = 10;
+    while (i) {
+        printf("%d seconds left\n", i--);
         sleep(1); // pause for 1 sec
     }
+    printf("Program has terminated\n");
 }
