696fe36da07b8a33f1d2bbe1da48362762038e25
Jerry Wu <jwu1100@gmail.com>
2017-05-24 14:01:46 -1000

    initial commit c++ tutorial

---
+++ practice_c++/1_hello_world.cpp
@@ -0,0 +1,15 @@
+/*
+   Hello world! I'm brushing up on my C++ skills and to do so, I'll be going
+   through a series of tutorial programs.
+
+   For reference, I am following this: http://www.cplusplus.com/doc/tutorial/
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "I am a C++ program." << std::endl;
+
+    return 0;
+}
