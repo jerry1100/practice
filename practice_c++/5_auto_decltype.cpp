544fa8067905d8cc8101d7d4588d37ada38a5195
Jerry Wu <jwu1100@gmail.com>
2017-06-16 13:13:15 -1000

    fixed wording

--- practice_c++/5_auto_decltype.cpp
+++ practice_c++/5_auto_decltype.cpp
@@ -1,13 +1,13 @@
 /*
-   We can use 'auto' or 'decltype' to initialize or declare a new variable with
-   the same type as a previous variable. For example, consider an integer named
-   'a' with the value 5. To initialize another integer 'b' to the value of 'a',
-   we know we could do 'int b = a;' but we could also do 'auto b = a;'. Using
-   'auto' will tell the compiler to figure out the type of 'a' and initialize
-   'b' with the same type. This may be useful if the type is a long word and is
-   hard to type out, or if one wants to change the type of 'a' frequently and
-   doesn't want to have to update all the instances of 'b'. Also note that
-   'decltype' works in a similar way.
+   We can use 'auto' or 'decltype' to initialize or declare a new variable
+   without having to explicitly state its type. We accomplish this by using an
+   existing variable and telling the compiler to initialize or declare our new
+   variable with the same type as our existing variable.
+
+   The difference between 'auto' and 'decltype' is that 'auto' is used to
+   initialize a new variable using the type and value of an existing variable,
+   whereas 'decltype' just declares a new variable using the type of an
+   existing variable.
  */
 
 #include <iostream>
